4f37fcf18758a671b397dfee7ea5ec1ed77a7fd1
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-11-01 22:57:17 +0530

    authcontroller me bcrypt use kiya

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 export const signup =async (req,res)=>{
     const {fullName,email,passward} = req.body
 
@@ -14,8 +16,36 @@ export const signup =async (req,res)=>{
             return res.status(400).json({message:"Invalid email format"})
         }
 
-        
-    }catch(error){
+    const user= await User.findOne({
+        email:email.toLowerCase()
+});
+    if(user){
+        return res.status(400).json({message:"Email is already registered"})
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassward = await bcrypt.hash(passward,salt);
 
+    const newUser = new User({
+        fullName,
+        email:email.toLowerCase(),
+        passward:hashedPassward
+    });
+    if(newUser){
+        generateToken(newUser._id,res);
+        await newUser.save();
+        res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            email:newUser.email,
+            profilePic:newUser.profilePic,
+            createdAt:newUser.createdAt,
+            updatedAt:newUser.updatedAt,
+        });
+    }else{
+        res.status(500).json({message:"Something went wrong. Please try again later."})
+    }
+    }catch(error){
+        console.log("Error in signup controller",error);
+        res.status(500).json({message:"Server Error"});
     }
 }

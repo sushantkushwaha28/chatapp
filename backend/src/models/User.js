7d4c67036ee4bde3553499018b21c5f63d8a2db1
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-11-01 22:47:42 +0530

    User schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    fullName:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    profilePic:{
+        type:String,
+        default:""
+    }
+},{timestamps:true}); // timestamps will automatically add createdAt and updatedAt fields
+
+const User = mongoose.model("User",userSchema);
+export default User;

7e9de54e19b594d0ff7066e05dc3789370c992aa
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-11-22 23:40:27 +0530

    gettoken

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,5 +1,13 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId,res)=>{
-    
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
 }
